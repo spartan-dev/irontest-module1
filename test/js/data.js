41693b697b87c4351514111d8aba754e2cd05872
maerd19 <revolutions_333@hotmail.com>
2019-09-16 14:16:59 -0500

    done

--- test/js/data.js
+++ test/js/data.js
@@ -1,65 +1,84 @@
 // Characters
- /**
-
-    name: "Rick Sanchez"
-    status: "Alive"
-    species: "Human"
-    type: ""
+let Rick = {
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    type: "",
     gender: "Male"
+}
 
-
-    name: "Morty Smith"
-    status: "Alive"
-    species: "Human"
-    type: ""
+let Morty = {
+    name: "Morty Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
     gender: "Male"
+}
 
-    name: "Summer Smith"
-    status: "Alive"
-    species: "Human"
-    type: ""
+let Summer = {
+    name: "Summer Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
     gender: "Female"
+}
 
-    name: "Beth Smith"
-    status: "Alive"
-    species: "Human"
-    type: ""
+let Beth = {
+    name: "Beth Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
     gender: "Female"
+}
 
-    name: "Jerry Smith"
-    status: "Alive"
-    species: "Human"
-    type: ""
+let Jerry = {
+    name: "Jerry Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
     gender: "Male"
+}
 
-    name: "Abadango Cluster Princess"
-    status: "Alive"
-    species: "Alien"
-    type: ""
+let Abadango = {
+    name: "Abadango Cluster Princess",
+    status: "Alive",
+    species: "Alien",
+    type: "",
     gender: "Female"
+}
 
-    name: "Abradolf Lincler"
-    status: "unknown"
-    species: "Human"
-    type: "Genetic experiment"
+let Abradolf = {
+    name: "Abradolf Lincler",
+    status: "unknown",
+    species: "Human",
+    type: "Genetic experiment",
     gender: "Male"
+}
 
-    name: "Adjudicator Rick"
-    status: "Dead"
-    species: "Human"
-    type: ""
+let Adjudicator = {
+    name: "Adjudicator Rick",
+    status: "Dead",
+    species: "Human",
+    type: "",
     gender: "Male"
+}
 
-    name: "Agency Director"
-    status: "Dead"
-    species: "Human"
-    type: ""
+let Agency = {
+    name: "Agency Director",
+    status: "Dead",
+    species: "Human",
+    type: "",
     gender: "Male"
+}
 
-    name: "Alan Rails"
-    status: "Dead"
-    species: "Human"
-    type: "Superhuman (Ghost trains summoner)"
+let Alan = {
+    name: "Alan Rails",
+    status: "Dead",
+    species: "Human",
+    type: "Superhuman (Ghost trains summoner)",
     gender: "Male"
-  
-  */
+}
+
+let charactersArray = [
+  Rick, Morty, Summer, Beth, Jerry, Abadango, Abradolf, Adjudicator, Agency, Alan
+];  
--- test/js/main.js
+++ test/js/main.js
@@ -1,42 +1,56 @@
 //new array
+console.log(charactersArray);
 
 
 //Get the first three elements of the Array without altering the original Array
 getFirtsThreeElements=()=>{
-    //...write your code here
-
-    console.log('The first three',/** here your new answer*/)
+    let firstThree = charactersArray.slice(0, 3);
+    console.log('The first three',firstThree);
 }
-//Find all the characters whose species is Aliens nad return in new array
+//Find all the characters whose species is Aliens and return in new array
 getAliens=()=>{
-    //...write your code here
-    console.log('the alien is:',/** here your new answer*/)
+    let onlyAliens = charactersArray.filter(el => el.species != 'Human');
+    console.log('the alien is:', onlyAliens);
 }
-//create a new character and add it to the matrix and add consecutive identifiers (1,2, ... n) to all characters
+//create a new character and add it to the matrix; add consecutive identifiers (1,2, ... n) to all characters
 newCharacter=()=>{
-    //...write your code here
-
-    console.log('New Charanter and id to all:',/** here your new answer*/)
+    let charClone = [...charactersArray];
+    let Bird = {
+        name: "Bird Person",
+        status: "Dead",
+        species: "Bird",
+        type: "",
+        gender: "Male"
+    }
+    charClone.push(Bird);
+    charactersIndex = charClone.forEach( (el, idx) => el.id = idx + 1);
+    console.log('New Character and id to all:',charClone);
 }
 //find the family smith (Morty, Beth, Summer, Jerry, Rick)  in a new Array called familySmith
 getFamilySmith=()=>{
-    //...write your code here
-
-    console.log('Family Smith:',/** here your new answer*/)
+    let familySmith = charactersArray.filter(el => el.name.includes('Sanchez') || el.name.includes('Smith'));
+    console.log('Family Smith:', familySmith);
 }
 //Jerry and Beth have divorced, remove Jerry from the Array Smith Family, and get a random family member and print all their values
 familyBroken=()=>{
-    //...write your code here
-
-    console.log('Family without Jerry',/** here your new answer*/)
-    console.log('Random member name:, status:, species:, type:, gender:')
+    let familySmith = charactersArray.filter(el => el.name.includes('Sanchez') || el.name.includes('Smith'));
+    let familyBroken = familySmith.filter(el => !el.name.includes('Jerry'));
+    let randomMember = familyBroken[Math.floor(Math.random() * familyBroken.length)];
+    let {name, status, species, type, gender} = randomMember;
+    console.log('Family without Jerry',familyBroken);    
+    console.log(`Random member name:${name}, status:${status}, species:${species}, type:${type}, gender:${gender}`)
 }
 //The Final test, Rick needs to have more order, the original Array creates a new object, in which you group by status, without altering the original
 orderArray=()=>{
-     //...write your code here
 
     console.log('new order',/** here your new answer*/)
 }
 
 
 //In this part we execute our functions
+getFirtsThreeElements();
+getAliens();
+newCharacter();
+getFamilySmith();
+familyBroken();
+// orderArray();
