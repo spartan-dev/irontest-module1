3733e5683743feb639d732c11b9b834c74aae9cd
Sarah Ortega <sarah.ortega.fuentes@gmail.com>
2022-01-24 20:29:25 -0600

    create array characters

--- test/js/main.js
+++ test/js/main.js
@@ -1,41 +1,99 @@
 //new array
-
-
+const charactersArray = [{
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+}, {
+    name: "Morty Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+}, {
+    name: "Summer Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Female",
+}, {
+    name: "Beth Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Female",
+}, {
+    name: "Jerry Smith",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+}, {
+    name: "Abadango Cluster Princess",
+    status: "Alive",
+    species: "Alien",
+    type: "",
+    gender: "Female",
+}, {
+    name: "Abradolf Lincler",
+    status: "unknown",
+    species: "Human",
+    type: "Genetic experiment",
+    gender: "Male",
+}, {
+    name: "Adjudicator Rick",
+    status: "Dead",
+    species: "Human",
+    type: "",
+    gender: "Male"
+}, {
+    name: "Agency Director",
+    status: "Dead",
+    species: "Human",
+    type: "",
+    gender: "Male",
+}, {
+    name: "Alan Rails",
+    status: "Dead",
+    species: "Human",
+    type: "Superhuman (Ghost trains summoner)",
+    gender: "Male",
+}]
 //Get the first three elements of the Array without altering the original Array
-getFirtsThreeElements=()=>{
-    //...write your code here
+getFirtsThreeElements = () => {
 
-    console.log('The first three',/** here your new answer*/)
+    console.log('The first three', /** here your new answer*/ )
 }
 //Find all the characters whose species is Aliens nad return in new array
-getAliens=()=>{
+getAliens = () => {
     //...write your code here
-    console.log('the alien is:',/** here your new answer*/)
+    console.log('the alien is:', /** here your new answer*/ )
 }
 //create a new character and add it to the matrix and add consecutive identifiers (1,2, ... n) to all characters
-newCharacter=()=>{
+newCharacter = () => {
     //...write your code here
 
-    console.log('New Charanter and id to all:',/** here your new answer*/)
+    console.log('New Charanter and id to all:', /** here your new answer*/ )
 }
 //find the family smith (Morty, Beth, Summer, Jerry, Rick)  in a new Array called familySmith
-getFamilySmith=()=>{
+getFamilySmith = () => {
     //...write your code here
 
-    console.log('Family Smith:',/** here your new answer*/)
+    console.log('Family Smith:', /** here your new answer*/ )
 }
 //Jerry and Beth have divorced, remove Jerry from the Array Smith Family, and get a random family member and print all their values
-familyBroken=()=>{
+familyBroken = () => {
     //...write your code here
 
-    console.log('Family without Jerry',/** here your new answer*/)
+    console.log('Family without Jerry', /** here your new answer*/ )
     console.log('Random member name:, status:, species:, type:, gender:')
 }
 //The Final test, Rick needs to have more order, the original Array creates a new object, in which you group by status, without altering the original
-orderArray=()=>{
-     //...write your code here
+orderArray = () => {
+    //...write your code here
 
-    console.log('new order',/** here your new answer*/)
+    console.log('new order', /** here your new answer*/ )
 }
 
 
